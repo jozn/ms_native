229b492508744ebc9be219ca82908ad3e30614e7
Hamid <7atashfeshan@gmail.com>
2016-09-16 04:26:14 +0430

    Cleanups of useless Classes

--- app/src/main/java/com/mardomsara/social/ui/views/ActiveView.java
+++
@@ -1,9 +0,0 @@
-package com.mardomsara.social.ui.views;
-
-/**
- * Created by Hamid on 6/12/2016.
- */
-public class ActiveView {
-
-
-}
--- app/src/main/java/com/mardomsara/social/ui/views/NewEmojiKeyBoard.java
+++
@@ -1,7 +0,0 @@
-package com.mardomsara.social.ui.views;
-
-/**
- * Created by Hamid on 9/13/2016.
- */
-public class NewEmojiKeyBoard {
-}
