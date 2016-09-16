4a6d3deb8d96f0d1a1f17446f1d2caa29a62d0eb
Hamid <7atashfeshan@gmail.com>
2016-09-16 04:39:52 +0430

    Cleanups of Base package #2

--- app/src/main/java/com/mardomsara/social/base/WSCall.java
+++ app/src/main/java/com/mardomsara/social/base/WSCall.java
@@ -3,12 +3,7 @@ package com.mardomsara.social.base;
 /**
  * Created by Hamid on 1/17/2016.
  */
-//TODO implement WSResCommand
 public class WSCall {
-//    public String Status;//dep
-//    public Object Payload; //,interface{}//dep
-//    public int ResTime;//dep
     public Command[] Commands;//[]WSResCommand
     public long SyncedNanoId;
-//    public String RequestId;//dep
 }
