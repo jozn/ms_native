40773e872b397cfcc940ded30442917f2f61814e
Hamid <7atashfeshan@gmail.com>
2016-10-16 03:04:57 +0330

    Clean ups of old events +2

--- app/src/main/java/com/mardomsara/social/models/events/CommandsReceivedToServer.java
+++
@@ -1,8 +0,0 @@
-package com.mardomsara.social.models.events;
-
-/**
- * Created by Hamid on 9/12/2016.
- */
-public class CommandsReceivedToServer {
-    public Long[] CmdIds;
-}
--- app/src/main/java/com/mardomsara/social/models/events/MsgsSyncMetaDeletedFromServer.java
+++
@@ -1,7 +0,0 @@
-package com.mardomsara.social.models.events;
-
-/**
- * Created by Hamid on 6/5/2016.
- */
-public class MsgsSyncMetaDeletedFromServer extends MessageSyncMeta {
-}
