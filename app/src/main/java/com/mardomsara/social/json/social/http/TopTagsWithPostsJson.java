afabfcbd03e6569d150a5df2287592b866633f6a
Hamid <7atashfeshan@gmail.com>
2016-11-20 02:48:38 +0330

    SuggestionsTagsPresenter updete to http2 + fix bugs

--- app/src/main/java/com/mardomsara/social/json/social/http/TopTagsWithPostsJson.java
+++ app/src/main/java/com/mardomsara/social/json/social/http/TopTagsWithPostsJson.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * Created by Hamid on 8/23/2016.
  */
+@Deprecated
 public class TopTagsWithPostsJson extends HttpBaseProtocol {
     public List<TopTagsWithPostsRowJson> Payload;
 }
--- app/src/main/java/com/mardomsara/social/ui/presenter/social/SuggestionsTagsPresenter.java
+++ app/src/main/java/com/mardomsara/social/ui/presenter/social/SuggestionsTagsPresenter.java
@@ -12,11 +12,14 @@ import android.widget.TextView;
 import com.mardomsara.social.Nav;
 import com.mardomsara.social.R;
 import com.mardomsara.social.app.API;
+import com.mardomsara.social.base.Http.Http;
+import com.mardomsara.social.base.Http.Result;
 import com.mardomsara.social.base.HttpOld;
 import com.mardomsara.social.helpers.AndroidUtil;
 import com.mardomsara.social.helpers.AppUtil;
 import com.mardomsara.social.helpers.Helper;
 import com.mardomsara.social.helpers.JsonUtil;
+import com.mardomsara.social.json.HttpJsonList;
 import com.mardomsara.social.json.social.http.TopTagsWithPostsJson;
 import com.mardomsara.social.json.social.http.TopTagsWithPostsRowJson;
 import com.mardomsara.social.json.social.rows.PostRowJson;
@@ -61,26 +64,56 @@ public class SuggestionsTagsPresenter extends BasePresenter implements AppHeader
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                Helper.showMessage("re");
-                loadFromServer();
+                Helper.showDebugMessage("re");
+//                loadFromServer();
+				adaptor.reload();
             }
         });
-        loadFromServer();
+//        loadFromServer();
     }
 
-    private void loadFromServer() {
-        AndroidUtil.runInBackground(() -> {
-            HttpOld.Req req = new HttpOld.Req();
-            req.absPath = API.RECOMMEND_TOP_TAGS_DISCOVER;
-            HttpOld.Result res = HttpOld.get(req);
-            if (res.ok) {
-                AndroidUtil.runInUi(() -> {
-                    loadedPostsFromNet(res);
-                });
-            }
-        });
+    private void loadFromServer(int pageNum) {
+		Http.getPath("/v1/recommend/top_tags_discover")
+			.setFormParam("page",""+pageNum)
+			.doAsyncUi(result -> {
+
+				adaptor.nextPageIsLoaded();
+				refreshLayout.setRefreshing(false);
+
+				if(result.isOk()){
+					HttpJsonList<TopTagsWithPostsRowJson> data = Result.fromJsonList(result,TopTagsWithPostsRowJson.class);
+					if(data.isPayloadNoneEmpty()){
+						if(pageNum==1){
+
+						}
+						adaptor.list.addAll(data.Payload);
+						adaptor.notifyDataSetChanged();
+
+					}else{
+						adaptor.setHasMorePage(false);
+					}
+
+				}else {
+					adaptor.setHasMorePage(false);
+				}
+			});
     }
 
+	@Deprecated
+	private void loadFromServer_bk() {
+		AndroidUtil.runInBackground(() -> {
+			HttpOld.Req req = new HttpOld.Req();
+			req.absPath = API.RECOMMEND_TOP_TAGS_DISCOVER;
+			HttpOld.Result res = HttpOld.get(req);
+			if (res.ok) {
+				AndroidUtil.runInUi(() -> {
+					loadedPostsFromNet(res);
+				});
+			}
+		});
+	}
+
+	@Deprecated
     private void loadedPostsFromNet(HttpOld.Result res) {
         TopTagsWithPostsJson data = JsonUtil.fromJson(res.data, TopTagsWithPostsJson.class);
         if (data != null && data.Payload != null && data.Status.equalsIgnoreCase("OK")) {
@@ -94,7 +127,7 @@ public class SuggestionsTagsPresenter extends BasePresenter implements AppHeader
 
     @Override
     public void loadNextPage(int pageNum) {
-
+		loadFromServer(pageNum);
     }
 
     public static class TopTagsAdaptor extends AppHeaderFooterRecyclerViewAdapter<TagHolder> {
@@ -126,7 +159,6 @@ public class SuggestionsTagsPresenter extends BasePresenter implements AppHeader
         @Bind(R.id.image3) ImageView image3;
         @Bind(R.id.text) TextView text;
 
-
         int size =0;
 
         public TagHolder(View itemView) {
@@ -143,21 +175,32 @@ public class SuggestionsTagsPresenter extends BasePresenter implements AppHeader
             text.setText("#"+tagJson.Tag.Name);
 
             if(tagJson.Posts == null) return;
+
+			image1.setVisibility(View.GONE);
+			image2.setVisibility(View.GONE);
+			image3.setVisibility(View.GONE);
+
+			PostRowJson post;
 //            if(tagJson.Posts)
             if(tagJson.Posts.size()>0){
-                setImage(image1,tagJson.Posts.get(0).MediaUrl,tagJson.Tag.Name,tagJson.Posts.get(0));
+				post = tagJson.Posts.get(0);
+                setImage(image1, post.MediaUrl, tagJson.Tag.Name,post);
             }
 
             if(tagJson.Posts.size()>1){
-                setImage(image2,tagJson.Posts.get(1).MediaUrl,tagJson.Tag.Name,tagJson.Posts.get(0));
+				post = tagJson.Posts.get(1);
+                setImage(image2, post.MediaUrl, tagJson.Tag.Name,post);
             }
 
             if(tagJson.Posts.size()>2){
-                setImage(image3,tagJson.Posts.get(2).MediaUrl,tagJson.Tag.Name,tagJson.Posts.get(0));
+				post = tagJson.Posts.get(2);
+                setImage(image3, post.MediaUrl, tagJson.Tag.Name,post);
             }
         }
 
         void setImage(ImageView image, String src, String tag, PostRowJson post) {
+			image.setVisibility(View.VISIBLE);
+			//todo extract this url
             src = "http://localhost:5000/"+src;
             AppUtil.log("SCC2" +src);
             if(src.equals("")) return;
@@ -173,8 +216,6 @@ public class SuggestionsTagsPresenter extends BasePresenter implements AppHeader
 
             image.setOnClickListener(onClick);
         }
-
-
     }
 
 }
