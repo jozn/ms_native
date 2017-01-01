151962f555351421afcd7b87f5c8f08d5eab41bd
Hamid <7atashfeshan@gmail.com>
2017-01-02 01:44:25 +0330

    X.java + some code play

---
+++ app/src/main/java/com/mardomsara/social/play/Play_RV.java
@@ -0,0 +1,27 @@
+package com.mardomsara.social.play;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hamid on 12/30/2016.
+ */
+
+public class Play_RV extends RecyclerView.Adapter {
+
+	@Override
+	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		return null;
+	}
+
+	@Override
+	public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+	}
+
+	@Override
+	public int getItemCount() {
+		return 0;
+	}
+}
