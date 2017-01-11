89f40c68a00d53ba06e5001a5a1ea279c5e2ea53
Hamid <7atashfeshan@gmail.com>
2017-01-11 13:00:08 +0330

    add furure.txt

--- app/src/main/java/com/mardomsara/social/base/Http/Req.java
+++ app/src/main/java/com/mardomsara/social/base/Http/Req.java
@@ -89,7 +89,7 @@ public class Req<T> {
 		}
 	}
 
-    //generics dosn't work, just setOrReplace for futuer maybe
+    //generics dosn't work, just setOrReplace for future.txt maybe
     public <K> Req doAsync(CallBack<K> callBack){
         if(action == Action.DOWNLOAD){
             throw new IllegalArgumentException("In Http2 for uploads actions doAsyncDownload(...) must be called in doAsyncDownload(), not doAsync(...)");
