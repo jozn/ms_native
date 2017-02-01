65a52eb682112dfb9993d4f1a34b9f51de0c0940
Hamid <7atashfeshan@gmail.com>
2017-02-01 18:23:31 +0330

    MsgImages cleanups

--- app/src/main/java/com/mardomsara/social/ui/cells/chats/msgs/MsgCell_PhotoMe.java
+++ app/src/main/java/com/mardomsara/social/ui/cells/chats/msgs/MsgCell_PhotoMe.java
@@ -19,7 +19,6 @@ public class MsgCell_PhotoMe extends MsgCell_AbstractViewHolder {
     public MsgCell_PhotoMe(X.Msg_RowImageMe xv) {
         super(xv.root);
 		x = xv;
-//        ButterKnife.bind(this, itemView);
     }
 
     public static MsgCell_PhotoMe makeNew(ViewGroup parent){
@@ -43,29 +42,3 @@ public class MsgCell_PhotoMe extends MsgCell_AbstractViewHolder {
 
 
 }
-/*//        URI uri = AppUtil.(msg.getMediaLocalSrc());
-//        msg_image.
-//        Uri u = Uri.parse(msg.getMediaLocalSrc());
-        Uri u = Uri.parse("http://localhost:5000/public/photo/1_960.jpg");
-        File file = new File(msg.getMediaLocalSrc());
-//        int max_width = msg_content_holder.getLayoutParams().width;//px
-//        int max_width = msg_content_holder.getWidth();;//px
-
-        int max_width = (int) (AndroidUtil.getScreenWidth() * 0.8);
-        AppUtil.log("width: "+max_width+AndroidUtil.getScreenResolution()+AndroidUtil.getDensity());
-        max_width = AndroidUtil.pxToDp(max_width);
-//        ViewGroup.LayoutParams sizes = new ViewGroup.LayoutParams(AndroidUtil.dpToPx(msg.getMediaWidth()),
-//                AndroidUtil.dpToPx(msg.getMediaWidth()));
-//        msg_image.setLayoutParams(sizes);
-
-        ViewHelper.setImageSizesWithMaxPx(msg_image, max_width, msg.getMediaWidth(),msg.getMediaHeight());
-//        ViewHelper.setViewSizeDp(msg_image,msg.getMediaWidth(),msg.getMediaHeight());
-//        msg_image.setLayoutParams();
-        file.toURI();
-        Uri u2 =Uri.fromFile(file);
-//        msg_image.setAspectRatio(1.33f);
-        msg_image.setImageURI(u2);
-
-        msg_image.setOnClickListener((v)->{
-            new FullScreenImage();
-        });*/
