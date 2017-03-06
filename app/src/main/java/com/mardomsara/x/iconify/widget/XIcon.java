b0d917a61f9f8aa34f115aa95fa303b4741da56e
Hamid <7atashfeshan@gmail.com>
2017-03-06 08:56:36 +0330

    update dependency to android api 25 + Xicon

---
+++ app/src/main/java/com/mardomsara/x/iconify/widget/XIcon.java
@@ -0,0 +1,62 @@
+package com.mardomsara.x.iconify.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+import com.mardomsara.x.iconify.Iconify;
+import com.mardomsara.x.iconify.internal.HasOnViewAttachListener;
+
+public class XIcon extends AppCompatTextView implements HasOnViewAttachListener {
+
+    private HasOnViewAttachListenerDelegate delegate;
+
+    public XIcon(Context context) {
+        super(context);
+        init();
+    }
+
+    public XIcon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public XIcon(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        setTransformationMethod(null);
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(Iconify.compute(getContext(), text, this), type);
+    }
+
+
+
+    public void setFullText(CharSequence text){
+
+	}
+
+
+    @Override
+    public void setOnViewAttachListener(OnViewAttachListener listener) {
+        if (delegate == null) delegate = new HasOnViewAttachListenerDelegate(this);
+        delegate.setOnViewAttachListener(listener);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        delegate.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        delegate.onDetachedFromWindow();
+    }
+}
