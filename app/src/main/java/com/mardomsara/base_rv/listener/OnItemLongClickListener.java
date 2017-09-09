e94785c8152f604d08216a0828de697701e7b5ec
Hamid <7atashfeshan@gmail.com>
2017-09-09 16:44:40 +0430

    migrate BRVA to mardomsrar BRAVA

---
+++ app/src/main/java/com/mardomsara/base_rv/listener/OnItemLongClickListener.java
@@ -0,0 +1,32 @@
+package com.mardomsara.base_rv.listener;
+
+import android.view.View;
+
+import com.mardomsara.base_rv.BaseQuickAdapter;
+
+/**
+ * create by: allen on 16/8/3.
+ */
+
+public abstract class OnItemLongClickListener extends SimpleClickListener {
+    @Override
+    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+
+    }
+
+    @Override
+    public void onItemLongClick(BaseQuickAdapter adapter, View view, int position) {
+        onSimpleItemLongClick(adapter, view, position);
+    }
+
+    @Override
+    public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+
+    }
+
+    @Override
+    public void onItemChildLongClick(BaseQuickAdapter adapter, View view, int position) {
+    }
+
+    public abstract void onSimpleItemLongClick(BaseQuickAdapter adapter, View view, int position);
+}
