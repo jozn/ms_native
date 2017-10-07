95a5437b4d3966dc4e80a3662525787f6407d816
Hamid <7atashfeshan@gmail.com>
2017-10-07 13:21:18 +0330

    some cleanup of commented codes for msgEntry page

--- app/src/main/java/com/mardomsara/social/models_realm/helpers/HelperPushAndRefreshHandlerOfData.java
+++ app/src/main/java/com/mardomsara/social/models_realm/helpers/HelperPushAndRefreshHandlerOfData.java
@@ -44,32 +44,34 @@ public class HelperPushAndRefreshHandlerOfData {
 
 		Realm realm = AppRealm.getChatRealm();
 		for (PB_ChatView pb_chatView : pbChats) {
-			RealmChatView realmChatView = Database.getRealmChatView(realm, pb_chatView.getChatKey());
-			if (realmChatView == null) {//create new chat
+			try{
+				RealmChatView realmChatView = Database.getRealmChatView(realm, pb_chatView.getChatKey());
+				if (realmChatView == null) {//create new chat
 //				realmChatView = realm.createObject(RealmChatView.class);
-				realmChatView = RealmChatView.fromPB(pb_chatView);
-				//realmChatView.user = Database.getRealmUserView(realm, pb_chatView.getUserId());
+					realmChatView = RealmChatView.fromPB(pb_chatView);
+					//realmChatView.user = Database.getRealmUserView(realm, pb_chatView.getUserId());
 //				if (realmChatView.user == null);// continue;//user must set
 
-			} else {//update existing ine
+				} else {//update existing ine
 
+				}
+
+				if(pb_chatView.getUserView() != null){//must alwase be true
+					realmChatView.UserView = RealmUserView.fromPB(pb_chatView.getUserView());
+				}else {
+					AppUtil.log("pb: handling - realm users: " + pb_chatView.getUserView().getUserName());
+				}
+
+				realmChatView.LastMessage = RealmMessageViewHelper.getLastMessageForChat(AppRealm.getChatRealm(),realmChatView.RoomKey);
+				realmChatView.UnseenCount = RealmMessageViewHelper.getUnseenCountForChat(AppRealm.getChatRealm(),realmChatView.ChatKey,realmChatView.LastSeenMessageId);
+
+				RealmChatView chatView2 = realmChatView;
+				realm.executeTransaction((r)->{
+					r.copyToRealmOrUpdate(chatView2);
+				});
+			}catch (Exception e){
+				e.printStackTrace();
 			}
-
-			if(pb_chatView.getUserView() != null){//must alwase be true
-				realmChatView.UserView = RealmUserView.fromPB(pb_chatView.getUserView());
-			}else {
-				AppUtil.log("pb: handling - realm users: " + pb_chatView.getUserView().getUserName());
-			}
-
-			realmChatView.LastMessage = RealmMessageViewHelper.getLastMessageForChat(AppRealm.getChatRealm(),realmChatView.RoomKey);
-			realmChatView.UnseenCount = RealmMessageViewHelper.getUnseenCountForChat(AppRealm.getChatRealm(),realmChatView.ChatKey,realmChatView.LastSeenMessageId);
-
-			RealmChatView chatView2 = realmChatView;
-			realm.executeTransaction((r)->{
-				r.copyToRealmOrUpdate(chatView2);
-			});
-
-
 		}
 		realm.close();
 	}
@@ -77,18 +79,22 @@ public class HelperPushAndRefreshHandlerOfData {
 	public static void newMessageViewList(List<PB_MessageView> chats){
 		if(chats == null || chats.size() ==0 ) return;
 
-		List<RealmMessageView> msgs = new ArrayList();
-		for (PB_MessageView pbMessageView :chats) {
-			RealmMessageView t = RealmMessageView.fromPB(pbMessageView);
-			if(pbMessageView.getMessageFileView() != null){
-				t.MessageFileView = RealmMessageFileView.fromPB(pbMessageView.getMessageFileView());
+		try {
+			List<RealmMessageView> msgs = new ArrayList();
+			for (PB_MessageView pbMessageView :chats) {
+				RealmMessageView t = RealmMessageView.fromPB(pbMessageView);
+				if(pbMessageView.getMessageFileView() != null){
+					t.MessageFileView = RealmMessageFileView.fromPB(pbMessageView.getMessageFileView());
+				}
+				msgs.add(t);
 			}
-			msgs.add(t);
-		}
 
-		AppRealm.getChatRealm().executeTransaction((trans) -> {
-			trans.copyToRealmOrUpdate(msgs);
-		});
+			AppRealm.getChatRealm().executeTransaction((trans) -> {
+				trans.copyToRealmOrUpdate(msgs);
+			});
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	public static void userViewList(List<PB_UserView> chats){}
